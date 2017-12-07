79c3945f5d4a588e4a3e0ebc80ae8a1799ae5a05
Scott Moschella <scottmosch@gmail.com>
2017-12-07 12:11:09 -0800

    Configure webpack for Sass and React

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin(
+  "./dist/css/style.min.css"
+);
+
+module.exports = {
+  entry: [
+    './src/Main.jsx'
+  ],
+  plugins: [
+    new UglifyJSPlugin({
+      test: /\.js($|\?)/i,
+      sourceMap: true
+    }),
+    extractSass
+  ],
+  output: {
+    filename: './dist/js/bundle.min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /(node_modules)/,
+        use: extractSass.extract({
+          use: [{
+              loader: "css-loader?minimize", // translates CSS into CommonJS          
+            },
+            {
+              loader: "sass-loader" // creates style nodes from JS strings
+            }]
+        })
+      }
+    ]
+  }
+};
